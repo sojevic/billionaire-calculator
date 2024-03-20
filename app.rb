26e1bbf374fcfd3f001768729539878bdf438d9b
sojevic <sojevic@gmail.com>
2024-03-20 12:23:53 +1100

    Fixed up Debts

--- app.rb
+++ app.rb
@@ -6,6 +6,7 @@ require_relative 'financial_data'
 include ActionView::Helpers::NumberHelper
 
 TAX_RATE = 25.0
+TABLE_YEARS = 25
 
 configure do
   $financial_data = FinancialData.new(0, 0, TAX_RATE)
@@ -15,13 +16,17 @@ get '/' do
   erb :index
 end
 
+############################################################
+####################### BUSINESSES #########################
+############################################################
+
 post '/add_business' do
   business_name = params[:business_name]
   initial_income = params[:initial_income].to_f
   growth_rates = (1..6).map { |i| params["income_growth_rate_#{i}"].to_f }
 
   $financial_data.add_business(business_name, initial_income, growth_rates)
-  $financial_data.calculate(50)
+  $financial_data.calculate(TABLE_YEARS)
 
   redirect '/'
 end
@@ -36,7 +41,7 @@ post '/calculate' do
   financial_data = FinancialData.new(initial_income, income_growth_rate, tax_rate)
 
   # Calculate the financial information for each year
-  financial_data.calculate(50)
+  financial_data.calculate(TABLE_YEARS)
 
   # Pass the calculated data to the results template
   erb :results, locals: { financial_data: financial_data }
@@ -55,7 +60,7 @@ post '/update_business/:index' do
   growth_rates = (1..6).map { |i| params["income_growth_rate_#{i}"].to_f }
 
   $financial_data.update_business(index, business_name, initial_income, growth_rates)
-  $financial_data.calculate(50)
+  $financial_data.calculate(TABLE_YEARS)
 
   redirect '/'
 end
@@ -63,10 +68,14 @@ end
 delete '/delete_business/:index' do
   index = params[:index].to_i
   $financial_data.delete_business(index)
-  $financial_data.calculate(50)
+  $financial_data.calculate(TABLE_YEARS)
 
   redirect '/'
 end
+
+############################################################
+########################## DEBTS ###########################
+############################################################
 
 post '/add_debt' do
   debt_amount = params[:debt_amount].to_f
@@ -75,7 +84,34 @@ post '/add_debt' do
   pay_back_asap = params[:pay_back_asap] == 'on'
 
   $financial_data.add_debt(debt_amount, interest_rate, repayment_period, pay_back_asap)
-  $financial_data.calculate(50)
+  $financial_data.calculate(TABLE_YEARS)
+
+  redirect '/'
+end
+
+get '/edit_debt/:index' do
+  index = params[:index].to_i
+  @debt = $financial_data.debts[index]
+  erb :edit_debt
+end
+
+post '/update_debt/:index' do
+  index = params[:index].to_i
+  debt_amount = params[:debt_amount].to_f
+  interest_rate = params[:interest_rate].to_f
+  repayment_period = params[:repayment_period].to_i
+  pay_back_asap = params[:pay_back_asap] == 'on'
+
+  $financial_data.update_debt(index, debt_amount, interest_rate, repayment_period, pay_back_asap)
+  $financial_data.calculate(TABLE_YEARS)
+
+  redirect '/'
+end
+
+delete '/delete_debt/:index' do
+  index = params[:index].to_i
+  $financial_data.delete_debt(index)
+  $financial_data.calculate(TABLE_YEARS)
 
   redirect '/'
 end
--- financial_data.rb
+++ financial_data.rb
@@ -24,64 +24,61 @@ class FinancialData
 
   def calculate(num_years)
     @data = []
-
+    debt_amounts = @debts.map { |debt| debt[:debt_amount] }
+    bank_balance = 0
+  
     num_years.times do |year|
-    business_incomes = @businesses.map do |business|
-      income = business[:initial_income]
-      (0..year).each do |y|
-        growth_rate = y < 5 ? business[:growth_rates][y] : business[:growth_rates][5]
-        income *= (1 + growth_rate / 100)
+      business_incomes = @businesses.map do |business|
+        income = business[:initial_income]
+        (0..year).each do |y|
+          growth_rate = y < 5 ? business[:growth_rates][y] : business[:growth_rates][5]
+          income *= (1 + growth_rate / 100)
+        end
+        { name: business[:name], income: income }
       end
-      { name: business[:name], income: income }
-    end
-
-    business_income_before_tax = business_incomes.sum { |income| income[:income] }
-    total_interest_payment = 0
-
-    @debts.each do |debt|
-      remaining_years = debt[:repayment_period] - year
-      if remaining_years > 0
-        interest_payment = debt[:debt_amount] * (debt[:interest_rate] / 100)
-        total_interest_payment += interest_payment
-        debt[:debt_amount] += interest_payment
-      end
-    end
-
-    business_income_before_tax -= total_interest_payment
-    business_income_after_tax = business_income_before_tax * (1 - @tax_rate / 100)
-
-    outstanding_debt = @debts.sum { |debt| debt[:debt_amount] }
-    debt_repayments = 0
-
-    @debts.each do |debt|
-      remaining_years = debt[:repayment_period] - year
-      if remaining_years > 0
-        if debt[:pay_back_asap] && business_income_after_tax >= debt[:debt_amount]
-          debt_repayments += debt[:debt_amount]
-          business_income_after_tax -= debt[:debt_amount]
-          debt[:debt_amount] = 0
-        elsif remaining_years == 1
-          debt_repayments += debt[:debt_amount]
-          business_income_after_tax -= debt[:debt_amount]
-          debt[:debt_amount] = 0
+  
+      business_income_before_tax = business_incomes.sum { |income| income[:income] }
+      total_interest_payment = 0
+      total_principal_payment = 0
+  
+      @debts.each_with_index do |debt, index|
+        remaining_years = debt[:repayment_period] - year
+        if remaining_years > 0
+          annual_payment = debt_amounts[index] * (debt[:interest_rate] / 100) / (1 - (1 + debt[:interest_rate] / 100)**(-remaining_years))
+          interest_payment = debt_amounts[index] * (debt[:interest_rate] / 100)
+          principal_payment = annual_payment - interest_payment
+  
+          total_interest_payment += interest_payment
+          total_principal_payment += principal_payment
+  
+          if debt[:pay_back_asap] && business_income_before_tax >= annual_payment
+            debt_amounts[index] -= principal_payment
+          else
+            debt_amounts[index] -= principal_payment
+          end
         end
       end
+  
+      business_income_before_tax -= total_interest_payment
+      business_income_after_tax = business_income_before_tax * (1 - @tax_rate / 100)
+  
+      outstanding_debt = debt_amounts.sum
+      debt_repayments = total_principal_payment
+  
+      bank_balance += business_income_after_tax - debt_repayments
+      new_investments = 0
+  
+      @data << {
+        business_incomes: business_incomes,
+        business_income_before_tax: business_income_before_tax,
+        business_income_after_tax: business_income_after_tax,
+        outstanding_debt: outstanding_debt,
+        debt_repayments: debt_repayments,
+        bank_balance: bank_balance,
+        new_investments: new_investments
+      }
     end
-
-    bank_balance = business_income_after_tax
-    new_investments = 0
-
-    @data << {
-      business_incomes: business_incomes,
-      business_income_before_tax: business_income_before_tax,
-      business_income_after_tax: business_income_after_tax,
-      outstanding_debt: outstanding_debt,
-      debt_repayments: debt_repayments,
-      bank_balance: bank_balance,
-      new_investments: new_investments
-    }
   end
-end
 
   def add_debt(debt_amount, interest_rate, repayment_period, pay_back_asap)
     @debts << {
@@ -91,4 +88,16 @@ end
       pay_back_asap: pay_back_asap
     }
   end
+
+  def update_debt(index, debt_amount, interest_rate, repayment_period, pay_back_asap)
+    @debts[index][:debt_amount] = debt_amount
+    @debts[index][:interest_rate] = interest_rate
+    @debts[index][:repayment_period] = repayment_period
+    @debts[index][:pay_back_asap] = pay_back_asap
+  end
+
+  def delete_debt(index)
+    @debts.delete_at(index)
+  end
 end
+
